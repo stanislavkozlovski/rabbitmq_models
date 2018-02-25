8c79254254bc0418f13b0198760745f14ac02d3e
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2018-02-25 11:04:29 +0200

    Specify dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,13 @@ setup(name='rabbitmq_models',
       url='https://github.com/Enether/rabbitmq_models',
       author='Stanislav Kozlovski',
       author_email='familyguyuser192@windowslive.com',
+      install_requires=['attrs==17.4.0',
+                        'dateutils==0.6.6',
+                        'pluggy==0.6.0',
+                        'py==1.5.2',
+                        'pytest==3.4.1',
+                        'python-dateutil==2.6.1',
+                        'pytz==2018.3',
+                        'six==1.11.0'
+                        ],
       packages=['rabbitmq_models'])
