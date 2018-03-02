fe45101553e6726806b86bfea8e897717e3fc2ee
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2018-03-02 07:56:12 +0200

    [HRME-5] Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rabbitmq_models',
-      version='0.1.2',
+      version='0.2.0',
       description='General models for the Harmonium project',
       url='https://github.com/Enether/rabbitmq_models',
       author='Stanislav Kozlovski',
