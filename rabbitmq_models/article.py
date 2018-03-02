e92507914bcceafefc17600eacee3d5305ca4f3c
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2018-03-02 07:55:42 +0200

    [HRME-5] Add site_name attribute to ReadArticle model

--- rabbitmq_models/article.py
+++ rabbitmq_models/article.py
@@ -5,10 +5,11 @@ from rabbitmq_models.base_model import BaseModel
 
 class ReadArticle(BaseModel):
     EXCHANGE_NAME = 'articles.read'
-    REQUIRED_ATTRS = ['title', 'read_at']
+    REQUIRED_ATTRS = ['title', 'read_at', 'site_name']
     ACCEPTED_ATTRS = REQUIRED_ATTRS + ['url']
 
-    def __init__(self, title, read_at, url=None):
+    def __init__(self, site_name: str, title: str, read_at: str, url=None):
+        self.site_name = site_name
         self.title = title
         self.url = url
         self.read_at = dateutil.parser.parse(read_at)
